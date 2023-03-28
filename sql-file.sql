25454272dfa0d63f74c9665dccb974ca71ef78a5
Yusuf Sezer <yusufsezer@mail.com>
2023-03-28 22:23:33 +0300

    upgraded dependency version

--- sql-file.sql
+++ sql-file.sql
@@ -11,4 +11,4 @@ CREATE TABLE IF NOT EXISTS `users` (
 	CONSTRAINT UK_user_email UNIQUE(user_email)
 );
 
-INSERT INTO users VALUES(1, 'yusufsezer@mail.com', MD5('123456'), 'Yusuf SEZER');
+INSERT INTO users VALUES(1, 'yusufsezer@mail.com', MD5('123456'), 'Yusuf Sezer');
